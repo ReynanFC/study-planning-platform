aea6ab265acad69de84b6df3f80ee9cee8be9a02
jamilly_ferreira <jamillyferreira039@gmail.com>
2026-03-19 18:58:39 -0400

    chore: initial project setup

---
+++ study-planning-platform/src/main/java/com/study/study_planning_platform/StudyPlanningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.study.study_planning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlanningPlatformApplication.class, args);
+	}
+
+}
---
+++ study-planning-platform/src/test/java/com/study/study_planning_platform/StudyPlanningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.study_planning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlanningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
