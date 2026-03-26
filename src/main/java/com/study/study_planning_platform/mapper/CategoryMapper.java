afa73ea8bf3acff0d53a7ef87dea339011aabff0
Reynan <reynandan11@gmail.com>
2026-03-26 20:21:40 -0300

    feat: created categoryMapper

---
+++ src/main/java/com/study/study_planning_platform/mapper/CategoryMapper.java
@@ -0,0 +1,26 @@
+package com.study.study_planning_platform.mapper;
+
+import com.study.study_planning_platform.dto.request.CategoryRequestDTO;
+import com.study.study_planning_platform.dto.response.CategoryResponseDTO;
+import com.study.study_planning_platform.entities.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface CategoryMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "tasks", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    Category toEntity(CategoryRequestDTO requestDTO);
+
+    CategoryResponseDTO toResponseDTO(Category category);
+
+    List<CategoryResponseDTO> toResponseDTO(List<Category> categories);
+}
---
+++ src/main/java/com/study/study_planning_platform/mapper/TaskMapper.java
@@ -0,0 +1,10 @@
+package com.study.study_planning_platform.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface TaskMapper {
+}
