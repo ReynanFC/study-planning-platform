bc33ea0a7038d511f827ab511b7ea5dce11ae012
Reynan <reynandan11@gmail.com>
2026-03-26 19:50:40 -0300

    build: refactor pom.xml and mapStruct addition

--- src/main/java/com/study/study_planning_platform/repository/UserRepository.java
+++ src/main/java/com/study/study_planning_platform/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.study.study_planning_platform.repository;
 
-import com.study.study_planning_platform.entities.Task;
 import com.study.study_planning_platform.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Task, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }
