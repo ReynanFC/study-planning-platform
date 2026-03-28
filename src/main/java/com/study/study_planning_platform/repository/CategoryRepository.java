991fd9a79b01b4626497e2c9d3f318d1608e5a40
Reynan <reynandan11@gmail.com>
2026-03-28 09:46:35 -0300

    refactor: adjustments to repositories

--- src/main/java/com/study/study_planning_platform/repository/CategoryRepository.java
+++ src/main/java/com/study/study_planning_platform/repository/CategoryRepository.java
@@ -1,13 +1,14 @@
 package com.study.study_planning_platform.repository;
 
 import com.study.study_planning_platform.entities.Category;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Page<Category> findByUserId(Long userId, Pageable pageable);
+    Optional<Category> findByIdAndUserId(Long id, Long userId);
+
 }
--- src/main/java/com/study/study_planning_platform/repository/TaskRepository.java
+++ src/main/java/com/study/study_planning_platform/repository/TaskRepository.java
@@ -1,14 +1,18 @@
 package com.study.study_planning_platform.repository;
 
 import com.study.study_planning_platform.entities.Task;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    Page<Task> findByUserId(Long userId, Pageable pageable);
+    Optional<Task> findByIdAndUserId(Long id, Long userId);
     Page<Task> findByCategoryIdAndUserId(Long categoryId, Long userId, Pageable pageable);
+    boolean existsByCategoryId(Long categoryId);
 }
