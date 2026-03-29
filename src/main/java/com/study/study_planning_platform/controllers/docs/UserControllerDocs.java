14811e9135721ddff19c12b5bb4f925aedf90e54
Reynan <reynandan11@gmail.com>
2026-03-29 19:14:45 -0300

    feat: created getUser

---
+++ src/main/java/com/study/study_planning_platform/controllers/docs/UserControllerDocs.java
@@ -0,0 +1,30 @@
+package com.study.study_planning_platform.controllers.docs;
+
+import com.study.study_planning_platform.dto.response.UserResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "User", description = "Endpoints for user profile")
+public interface UserControllerDocs {
+
+    @Operation(
+            summary = "Get current user",
+            description = "Returns the authenticated user profile",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<UserResponseDTO> getUser();
+}
