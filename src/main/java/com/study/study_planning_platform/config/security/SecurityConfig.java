fade845390f04a004f87e9336c3a54f2be7fecd3
Reynan <reynandan11@gmail.com>
2026-03-29 19:03:08 -0300

    config: add requestMatcher of the swagger

--- src/main/java/com/study/study_planning_platform/config/security/SecurityConfig.java
+++ src/main/java/com/study/study_planning_platform/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated()
