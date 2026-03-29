110d076a4fcdacefa04598ccb93e69dce0cead89
Reynan <reynandan11@gmail.com>
2026-03-29 18:31:36 -0300

    build: implementation authControllerDocs

--- src/main/java/com/study/study_planning_platform/controllers/docs/AuthControllerDocs.java
+++ src/main/java/com/study/study_planning_platform/controllers/docs/AuthControllerDocs.java
@@ -1,4 +1,50 @@
 package com.study.study_planning_platform.controllers.docs;
 
+import com.study.study_planning_platform.dto.request.UserLoginRequestDTO;
+import com.study.study_planning_platform.dto.request.UserRegistrationRequestDTO;
+import com.study.study_planning_platform.dto.response.UserResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Auth", description = "Endpoints for authentication")
 public interface AuthControllerDocs {
+
+    @Operation(
+            summary = "Register a new user",
+            description = "Creates a new user account",
+            tags = {"Auth"},
+            responses = {
+                    @ApiResponse(
+                            description = "Created",
+                            responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = UserResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Conflict", responseCode = "409", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<UserResponseDTO> register(@RequestBody UserRegistrationRequestDTO dto);
+
+    @Operation(
+            summary = "Login",
+            description = "Authenticates the user and returns a JWT token",
+            tags = {"Auth"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = String.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<String> login(@RequestBody UserLoginRequestDTO dto);
 }
